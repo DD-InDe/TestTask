023ba35faae73d21375cf63fa05846dad1318d5b
deer <bogdanovich_7712@mail.ru>
2024-10-11 16:28:17 +0300

    Initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿string url = "http://host.docker.internal:8080/Controller1";
+HttpClient client = new HttpClient();
+
+await GetState();
+
+async Task GetState()
+{
+    try
+    {
+        var message = await client.GetAsync(url);
+        Console.WriteLine(await message.Content.ReadAsStringAsync());
+    }
+    catch (Exception e)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+        Console.WriteLine($"Exception: {e.Message}");
+        await GetState();
+    }
+}
